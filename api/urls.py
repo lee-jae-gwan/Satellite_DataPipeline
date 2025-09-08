e15b1928289f546b4b7264ac63cac48b8e54c427
lee-jae-gwan <60457678+lee-jae-gwan@users.noreply.github.com>
2025-09-08 09:54:31 +0900

    second commit

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from .views import UploadSatelliteImage
 from .views import index_page
+from .views import get_presigned_url
+from .views import get_presigned_folderurl
 
 urlpatterns = [
     path('upload/', UploadSatelliteImage.as_view(), name= 'upload-satellite'),
     path('',index_page, name = 'index_page'),
+    path('get_presigned_url/', get_presigned_url, name='get_presigned_url'),
+    path('get_presigned_folderurl/', get_presigned_folderurl, name='get_presigned_folderurl'),
 ]
 
 
